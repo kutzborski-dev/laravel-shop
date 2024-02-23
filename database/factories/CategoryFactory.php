c63e978faf7bc95a4f9ccc1f40e3514b3309eacc
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-23 02:55:28 +0000

    (#4) Create Category factory, seeder and migration

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $categoryName = fake()->words(rand(2, 3));
+
+        return [
+            'name' => $categoryName,
+            'slug' => strtolower(str_ireplace(' ', '-', $categoryName)),
+            'is_active' => fake()->boolean()
+        ];
+    }
+}

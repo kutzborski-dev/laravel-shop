1852321c938a99440fbbcb426d9175c91b3894db
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-23 20:24:24 +0000

    (#11) Create basic layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Page title</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
 </head>
 <body>
+    @include('partials.header')
     
+    <main>
+        @yield('content')
+    </main>
+    
+    @include('partials.footer')
 </body>
 </html>

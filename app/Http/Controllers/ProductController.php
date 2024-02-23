f372dbab16929780cc2b431a5d906feba4890180
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-23 02:58:57 +0000

    (#6) Create Product model, controller and request

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+}

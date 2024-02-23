db023009832bf4fdb78aa4b937ad9f39343320dd
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-23 13:26:49 +0000

    (#7) Populate and call seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Product;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::factory(5)->has(
+            // Create 20 Products for 5 categories
+            Product::factory(20)
+        )->has(
+            // Create 10 subcategories with 10 products each
+            Category::factory(10)->has(
+                Product::factory(10)
+            , 'parent_id')
+        )->create();
     }
 }

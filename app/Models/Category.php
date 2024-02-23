dc8195772eeec5b6859d04fe4b6fd5b2c4642be1
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-23 13:16:50 +0000

    (#8) Create `Product` and `Category` relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
+
+    public function subCategories() {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parentCategory() {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
 }

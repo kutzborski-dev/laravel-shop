c8c457003d00ff8749e28486f7e98d812675efdf
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-23 12:48:02 +0000

    (#7) Make the `sku` field random data look more realistic

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,10 +16,15 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->words(rand(2, 4));
+        // Generate first letters to use for SKU based on product name
+        // Using array_map rather than turning into a Laravel collection as it would be over-kill for this use case alone
+        $firstLetters = implode('', array_map(fn($part) => $part[0], $name));
+
         return [
-            'name' => fake()->words(rand(2, 4), true),
+            'name' => implode(' ', $name),
             'model' => 'm-'. fake()->text(rand(2, 4)) . fake()->randomNumber(rand(4, 6), true),
-            'sku' => fake()->text(rand(2, 4)) . fake()->randomNumber(rand(4, 6), true),
+            'sku' => $firstLetters . fake()->randomNumber(rand(4, 6), true), // Create a somewhat realistic looking SKU
             'price' => fake()->randomFloat(2, 15, 200),
             'description' => fake()->paragraphs(4, true),
             'short_description' => fake()->paragraph(5),

e4f7c5e3b32554bbf849ecd73f2d10d8e8d0f12a
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-24 02:16:57 +0000

    (#13) Create basic product list partial

---
+++ resources/views/products/partials/product-list.blade.php
@@ -0,0 +1,6 @@
+<div class="product-list flex gap-x-8 flex-wrap justify-center">
+        <x-productcard />
+        <x-productcard />
+        <x-productcard />
+        <x-productcard />
+    </div>

baff28779290ce24f8a5777498e4a08bd947bd2b
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-24 02:19:37 +0000

    (#13) Add general container wrapper for content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,10 @@
     @include('partials.header')
     @include('partials.hero')
     
-    <main>
-        @yield('content')
+    <main class="py-16">
+        <div class="container mx-auto max-w-7xl px-4">
+            @yield('content')
+        </div>
     </main>
     
     @include('partials.footer')

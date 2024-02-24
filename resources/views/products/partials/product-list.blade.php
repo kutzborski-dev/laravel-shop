c2da926195f0bc1f74a12da4a184738248d41574
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-24 09:12:55 +0000

    (#18) Finish pagination

--- resources/views/products/partials/product-list.blade.php
+++ resources/views/products/partials/product-list.blade.php
@@ -1,5 +1,7 @@
-<div class="product-list grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-8">
+<div id="product-list" class="mb-5 grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-8">
     @foreach($products as $product)
         <x-productcard id="{{ $product->id }}" name="{{ $product->name }}" price="{{ $product->price }}" slug="{{ $product->slug }}" />
     @endforeach
 </div>
+
+{{ $products->links('vendor.pagination.tailwind', ['paginator' => $products]) }}

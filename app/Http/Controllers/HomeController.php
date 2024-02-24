d5e860ca052b1f37de01cae46ff3bab53a455caf
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-24 08:10:00 +0000

    (#16) Fix and populate homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Repositories\Product\ProductRepositoryInterface;
+use App\Http\Requests\ProductRequest;
+
 class HomeController extends Controller
 {
+    public function __construct(public ProductRepositoryInterface $productRepository) {
+    }
+
     public function index() {
-        return view('home');
+        $products = $this->productRepository->paginate(5);
+        return view('home', ['products' => $products]);
     }
 }

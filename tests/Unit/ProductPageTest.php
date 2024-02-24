3d2d024c1591bd5279a5caa781d5e70c3e39916e
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-24 08:35:34 +0000

    (#17) Add test script to check if page is reachable and has data

---
+++ tests/Unit/ProductPageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductPageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function the_page_is_reachable_and_has_data()
+    {
+        $product = \App\Models\Product::factory()->create();
+
+        $response = $this->get("/product/{$product->slug}-{$product->id}");
+        $response->assertStatus(200);
+
+        $response->assertStatus(200);
+        $response->assertSee(e($product->name));
+    }
+}

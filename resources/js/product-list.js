1cc069d7ab9835bc2bf7e430b2782c9fd04218b8
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-24 11:49:32 +0000

    Finish search bar toggle functionality

--- resources/js/product-list.js
+++ resources/js/product-list.js
@@ -1,5 +1,21 @@
+import { hasParent } from './utils';
+
 const sortButtons = document.querySelectorAll('a.sort-btn');
 
 sortButtons.forEach(btn => {
     btn.href = btn.href + '#product-list'
+});
+
+
+const searchBarToggle = document.querySelector('#search-bar-toggle');
+const searchBar = document.querySelector('#search-bar');
+
+searchBarToggle.addEventListener('click', function(){
+    searchBar.classList.toggle('hidden');
+});
+
+document.body.addEventListener('click', function(e) {
+    if(!searchBar.className.includes('hidden') && e.target != searchBar && !hasParent(searchBar, e.target) && e.target != searchBarToggle && !hasParent(searchBarToggle, e.target)) {
+        searchBar.classList.add('hidden');
+    }
 });
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     @include('partials.nav.mobile-menu')
 
   <header id="page-header" class="relative bg-white border-b border-gray-200">
-    <div id="search-bar" class="border border-gray-200 absolute left-[50%] -ml-[25%] top-full mt-0 max-w-7xl w-full bg-white flex items-center">
+    <div id="search-bar" class="border border-gray-200 absolute left-[50%] -ml-[25%] top-full mt-0 max-w-7xl w-full bg-white flex items-center shadow-sm hidden">
       <div class="mx-4">
         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -42,7 +42,7 @@
           <div class="ml-auto flex items-center">
             <!-- Search -->
             <div class="flex lg:ml-6 lg:mr-1 mr-2">
-              <a href="#" class="p-2 text-gray-400 hover:text-gray-500">
+              <a id="search-bar-toggle" href="#" class="p-2 text-gray-400 hover:text-gray-500">
                 <span class="sr-only">Search</span>
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />

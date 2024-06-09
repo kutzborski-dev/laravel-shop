420297777f59add7cb609fe2691b345770f8d374
Andre Kutzborski <andre.kutzborski@gmail.com>
2024-06-10 00:41:54 +0100

    Update hero banner text again

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,8 +1,8 @@
 <section class="bg-gray-100/35">
     <div class="grid max-w-7xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 sm:px-8">
         <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Join our Membership program</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">It's free to join! As a member, you'll earn points on purchases above £10, get free delivery on purchases above £30, enjoy other exclusive deals, and have any ongoing promotions or coupon codes automatically applied at checkout.</p>
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Join us today!</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">It's free to create an account! As a member, you'll earn points on purchases above £10, get free delivery on purchases above £35, enjoy other exclusive deals, and have any ongoing promotions or coupon codes automatically applied at checkout.</p>
             <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-gray-700 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-900">
                 Join membership
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>

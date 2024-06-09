1df196d27ed36ae68acfce6e18fe2ea164b4957c
Andre Kutzborski <andre.kutzborski@gmail.com>
2024-06-10 00:24:18 +0100

    Update placeholder text bar underneath hero banner

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -18,7 +18,7 @@
     <div class="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-800 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 w-full">
         <div class="flex flex-wrap items-center gap-x-4 gap-y-2 justify-center w-full">
             <p class="text-sm leading-6 text-gray-100">
-                <strong class="font-semibold"><a href="#" class="underline">Dolor sit amet</a></strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Nunc nisl turpis, mattis ac mi non, iaculis euismod risus.
+                <strong class="font-semibold"><a href="#" class="underline">Subscribe to our newsletter</a></strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>To stay up to date with new arrivals and not miss out on the best deals
             </p>
         </div>
     </div>

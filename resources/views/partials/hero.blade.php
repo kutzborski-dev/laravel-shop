8dd4d3985806b41dcda17ba7fefe0aedb445dce1
Andre Kutzborski <andre.kutzborski@gmail.com>
2024-07-06 16:45:28 +0100

    Hero section make over

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,22 +1,47 @@
-<section class="bg-gray-100/35">
-    <div class="grid max-w-7xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 sm:px-8">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Join us today!</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">It's free to create an account! As a member, you'll earn points on purchases above £10, get free delivery on purchases above £35, enjoy other exclusive deals, and have any ongoing promotions or coupon codes automatically applied at checkout.</p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                More details
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a> 
-        </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
-        </div>                
-    </div>
-    <div class="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-800 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 w-full">
-        <div class="flex flex-wrap items-center gap-x-4 gap-y-2 justify-center w-full">
-            <p class="text-sm leading-6 text-gray-100">
-                <strong class="font-semibold"><a href="#" class="underline">Subscribe to our newsletter</a></strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>To stay up to date with new arrivals and not miss out on the best deals
-            </p>
+<section class="bg-gray-100/35 relative pt-[29.875%] min-h-96">
+    <div class="absolute w-full h-full top-0 left-0">
+        <div class="h-full flex flex-col">
+            <div class="relative flex-1 flex">
+                <div class="absolute top-0 left-0 w-full h-full z-0 overflow-hidden">
+                    <picture>
+                        <source media="(max-width:468px)" srcset="{{ url('/images/kam-idris-HqHX3LBN18_574x696.jpg') }}">
+                        <source media="(max-width:642px)" srcset="{{ url('/images/kam-idris-HqHX3LBN18_640x425.jpg') }}">
+                        <source media="(max-width:1280px)" srcset="{{ url('/images/kam-idris-HqHX3LBN18_1280x849.jpg') }}">
+                        <source media="(max-width:1920px)" srcset="{{ url('/images/kam-idris-HqHX3LBN18_1920x1274.jpg') }}">
+                        <source media="(max-width:2560px)" srcset="{{ url('/images/kam-idris-HqHX3LBN18_2560x1699.jpg') }}">
+                        <img class="w-full h-auto absolute max-sm:-mt-[35.0625%] max-sm:translate-y-[0] max-sm:top-[52%] -mt-[50%] translate-y-[50%] max-md:translate-y-[55%] max-xs:-mt-[50%] max-xs:top-[50%]" src="{{ url('/images/kam-idris-HqHX3LBN18_3400x2256.jpg') }}" alt="Home hero banner image" />
+                    </picture>
+                </div>
+                <div class="relative sm:grid max-w-7xl mx-auto grid-cols-12 lg:px-8 md:px-0 z-1 max-xl:h-full hidden items-center">
+                    <div class="max-xl:h-full col-span-8 md:col-span-6 bg-gradient-to-r from-black/35 from-75% to-transparent to-100% lg:bg-black/40">
+                        <div class="max-xl:h-full mr-auto place-self-center w-[90%] lg:w-full px-6 py-6 justify-center flex flex-col">
+                            <h1 class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-4xl xl:text-5xl text-gray-100">Join us today!</h1>
+                            <p class="max-w-2xl mb-6 font-light text-gray-100 lg:mb-8 text-md lg:text-lg xl:text-xl">It's free to create an account! As a member, you'll earn points on purchases above £10, get free delivery on purchases above £35, enjoy other exclusive deals, and have any ongoing promotions or coupon codes automatically applied at checkout.</p>
+                            <a href="#" class="w-fit inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-200 border border-gray-200 rounded-lg hover:bg-gray-200 hover:text-gray-800 focus:ring-4 focus:ring-gray-300">
+                                More details
+                                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </a> 
+                        </div>
+                    </div>            
+                </div>
+            </div>
+            <div class="relative isolate flex items-center flex-center gap-x-6 overflow-hidden bg-gray-800 px-6 py-2.5 sm:px-3.5 w-full">
+                <div class="flex flex-wrap items-center gap-x-1 gap-y-2 justify-center w-full">
+                    <p class="text-sm leading-6 text-gray-100 max-md:flex max-md:flex max-md:flex-col max-md:text-center">
+                        <strong class="font-semibold max-md:text-lg">
+                            <a href="#" class="underline">Subscribe to our newsletter</a>
+                        </strong>
+                        <span class="max-md:hidden text-white" aria-hidden="true">
+                            <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current">
+                                <circle cx="1" cy="1" r="1" />
+                            </svg>
+                        </span>
+                        <span>
+                            To stay up to date with new arrivals and not miss out on the best deals
+                        </span>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
